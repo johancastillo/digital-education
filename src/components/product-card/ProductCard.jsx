e1af8b534967d845604fc1572066f1e2e4e0ef3a
Johan Castillo <jcjohan2707@gmail.com>
2021-01-16 09:53:43 -0400

    Icons added

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,12 +1,30 @@
 import "./ProductCard.css"
 
-const ProductCard = ({title, image}) => {
+const ProductCard = ({ title, image }) => {
     return (
-        <div class="card" style={{width: '100%'}}>
+        <div class="card" style={{ width: '100%' }}>
             <img src={image} class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">{title}</h5>
+
+                <div className="d-flex justify-content-between">
+                    <div className="box-icons">
+                        <span className="stars icon-star-full"></span>
+                        <span className="stars icon-star-full"></span>
+                        <span className="stars icon-star-full"></span>
+                        <span className="stars icon-star-half"></span>
+                        <span className="stars icon-star-void"></span>
+                    </div>
+
+                    <div>
+                        <span className="icon-bookmark-void"></span>
+                    </div>
+                </div>
+
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary " style={{ marginRight: '10px' }}>
+                    <span className="icon-cart"></span>
+                </a>
                 <a href="#" class="btn btn-primary">Comprar ahora</a>
             </div>
         </div>
