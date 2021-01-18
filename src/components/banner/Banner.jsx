57d5b116d761df192ed271a5466b0a062818d48e
Johan Castillo <jcjohan2707@gmail.com>
2021-01-18 12:55:33 -0400

    Change in the Banner Component

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,24 +1,26 @@
-import banner from './banner.png'
+import banner from './banner.webp'
 import "./Banner.css"
 
 const Banner = () => {
     return (
         <div className="banner">
-            <img className="banner-img" src={banner} alt=""  />
-            
-            <form className="form">
-                <h3>Transforma tus sueños y hazlo realidad</h3>
-                
-                <p>
-                    Mejora como profesional y adquiere los conocimientos que 
-                    necesitas para lograr tus metas.
-                </p>
+            <img className="banner-img" src={banner} alt="" />
 
-                <div class="mb-3">
-                    <input type="email" placeholder="¿Qué quieres aprender?" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
+            <div className="box-container">
+                <form className="form">
+                    <h3>Transforma tus sueños y hazlo realidad</h3>
 
-            </form>
+                    <p className="mt-4">
+                        Mejora como profesional y adquiere los conocimientos que
+                        necesitas para lograr tus metas.
+                    </p>
+
+                    <div class="mb-3">
+                        <input type="email" placeholder="¿Qué quieres aprender?" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 }
