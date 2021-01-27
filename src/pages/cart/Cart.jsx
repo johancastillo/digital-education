43622ccfcdd80418acf421aaa2b13e8963dd91d9
EuclidesG <euclidesgarrido95@gmail.com>
2021-01-26 21:30:26 -0400

    cambios en el carro de compra

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,17 +1,25 @@
+import CardShopping from "../../components/card-shopping/card-shopping"
 import "./Cart.css"
+import {useContext} from "react"
+import AppContext from "../../context/AppContext"
 
 const Cart = () => {
+    const {cartProducts} = useContext (AppContext)
     return (
     <div className="cart-page" >
     <h1> Carrito de compras </h1>
 
-    
-    
+    {
+      cartProducts.map(
+          producto => (<div> {producto} <div/>)
+        )
+    }
+    <CardShopping/> 
     </div>
     
 
 
-    )
+    
 
     
 } 
