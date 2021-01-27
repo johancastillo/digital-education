b00024e1870bdefd4c0a49fd5f5b15898ea3d7fc
Johan Castillo <jcjohan2707@gmail.com>
2021-01-26 22:50:40 -0400

    Shopping cart and total price

--- src/components/card-shopping/card-shopping.jsx
+++ src/components/card-shopping/card-shopping.jsx
@@ -1,20 +1,25 @@
-const CardShopping = () => {
+const CardShopping = ({ image, title, price }) => {
   return (
-    <div className="card mb-3" style={{maxWidth: '540px'}}>
+    <div className="card mb-3" style={{ maxWidth: '100%' }}>
       <div className="row g-0">
         <div className="col-md-4">
-          <img src="..." alt="..." />
+          <img src={image} alt="..."  style={{width: '100%'}} />
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">
+              {title}
+            </h5>
+
             <p className="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
               longer.
             </p>
             <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">
+                {`$${price} USD`}
+              </small>
             </p>
           </div>
         </div>
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,28 +1,72 @@
 import CardShopping from "../../components/card-shopping/card-shopping"
 import "./Cart.css"
-import {useContext} from "react"
+import { useContext, useEffect, useState } from "react"
 import AppContext from "../../context/AppContext"
+import axios from 'axios'
 
 const Cart = () => {
-    const {cartProducts} = useContext (AppContext)
+    const { cartProducts, setLoading } = useContext(AppContext)
+    const [courses, setCourses] = useState([])
+    const [total, setTotal] = useState(0)
+
+    // Get data with Axios
+    useEffect(() => {
+        setLoading(true)
+        axios.get('https://johancastillo.github.io/json-api-fake/digital-education/courses.json')
+            .then(
+                // Obteniendo los cursos
+                response => setCourses(response.data),
+                // Cambiando el estado del componente Loading a false
+                setLoading(false)
+            )
+            .catch(error => console.log(error))
+        }, [])
+        
+        let totalPrice = total
     return (
-    <div className="cart-page" >
-    <h1> Carrito de compras </h1>
+        <div className="cart-page px-4" >
+            <h1 className="text-center"> Carrito de compras </h1>
+            <p className="text-center">
+                {
+                    cartProducts.length === 0 ? "No hay productos en el carrito" : "Estos son los productos de tu carrito"
+                }
+            </p>
+            
 
-    {
-      cartProducts.map(
-          producto => (<div> {producto} <div/>)
-        )
-    }
-    <CardShopping/> 
-    </div>
-    
+            {
+                courses.map(
+                    course => {
+                        const validation = cartProducts.includes(course.id)
+
+                        if(validation){
+                            totalPrice += course.price                    
+                            return (
+                                <CardShopping 
+                                    title={course.title}
+                                    image={course.image}
+                                    price={course.price}
+                                />
+                            )
+                        }
+                    }
+                )
+            }    
+            
+            <h4 className="total p-4 text-center">
+                {
+                    totalPrice > 0 ? `Total: $${totalPrice} USD` : ""
+                }
+            </h4>
+
+            
+        </div>
+
+    )
 
 
-    
 
-    
-} 
+
+}
 
 export default Cart
 
